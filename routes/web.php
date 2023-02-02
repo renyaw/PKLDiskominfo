dbec9d9a3a3ad13a4a74f7d8d3030351ba182f23
renyaw <norarenada@students.undip.ac.id>
2023-02-02 23:41:02 +0700

    add routing all

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,35 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\kreditController;
-use App\Http\Controllers\kredit2Controller;
-use App\Http\Controllers\AkunController;
-use App\Http\Controllers\SKTMController;
-use App\Http\Controllers\KelurahanController;
-use App\Http\Controllers\KecamatanController;
-use App\Http\Controllers\DomisiliController;
-use App\Http\Controllers\EditDataController;
-use App\Http\Controllers\MasyarakatController;
-use App\Http\Controllers\StatMasyController;
+//All
+use App\Http\Controllers\buatAkunController;
+use App\Http\Controllers\loginController;
+//guest
+use App\Http\Controllers\berandaGuestController;
+//Masyarakat
+use App\Http\Controllers\profileController;
+use App\Http\Controllers\editProfileController;
+use App\Http\Controllers\dashMasyController;
+use App\Http\Controllers\formDomController;
+use App\Http\Controllers\formKredController;
+use App\Http\Controllers\formSKTM1Controller;
+use App\Http\Controllers\formSKTM2Controller;
+use App\Http\Controllers\kumpulanBerkasController;
+use App\Http\Controllers\riwayatController;
+use App\Http\Controllers\riwayatDomController;
+use App\Http\Controllers\riwayatKredController;
+use App\Http\Controllers\riwayatSKTMController;
+//Kelurahan
+use App\Http\Controllers\dashKelController;
+use App\Http\Controllers\antreanDomController;
+use App\Http\Controllers\antreanKredController;
+use App\Http\Controllers\antreanSKTMController;
+use App\Http\Controllers\verifKredController;
+use App\Http\Controllers\verifSKTMController;
+use App\Http\Controllers\verifDomController;
+//Kecamatan
+use App\Http\Controllers\dashKecController;
+use App\Http\Controllers\dashKec2Controller;
 
 
 /*
@@ -23,130 +42,49 @@ use App\Http\Controllers\StatMasyController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-// All
-Route::get('/signin', function () {
-    return view('signin');
-});
-
-Route::get('/', function () {https://drive.google.com/drive/u/0/folders/1o9fY_8y0e5w20aezfzSjXKbyqBQJW1dC
-    return view('guest/berandaguest');
-});
-
-Route::get('/signup', function () {
-    return view('masyarakat/signup');
-});
-
-Route::get('/beranda', function () {
-    return view('masyarakat/beranda');
-});
-
-Route::get('/sktm1', function () {
-    return view('masyarakat/sktm1');
-});
-
-Route::get('/sktm2', function () {
-    return view('masyarakat/sktm2');
-});
-
-Route::get('/dashboardpeg', function () {
-    return view('kelurahan/dashboardpeg');
-});
-
-Route::get('/usaha', function () {
-    return view('masyarakat/usaha');
-});
-
-Route::get('/domisili', function () {
-    return view('masyarakat/domisili');
-});
-
-Route::get('/berkas', function () {
-    return view('masyarakat/berkas');
-});
-
-Route::get('/verifkredit', [kreditController::class, 'index']);
-Route::get('/verifsktm', [SKTMController::class, 'index']);
-Route::get('/verifdom', [DomisiliController::class, 'index']);
-Route::get('/status', [StatMasyController::class, 'index']);
-Route::get('/datadiri', [EditDataController::class, 'index']);
-Route::get('/signup', [MasyarakatController::class, 'index']);
-
-Route::get('/berhasil', function () {
-    return view('masyarakat/berhasil');
-});
-
-Route::get('/datadiri', function () {
-    return view('masyarakat/datadiri');
-});
-
-Route::get('/kitas', function () {
-    return view('masyarakat/kitas');
-});
-
-Route::get('/lain', function () {
-    return view('masyarakat/lain');
-});
 
 
-Route::get('/lihatberkas', function () {
-    return view('/kelurahan/lihatberkas');
-});
+// Route::get('/signin', function () {
+//     return view('signin');
+// });
 
-//Route::get('/status', function () {
-//    return view('masyarakat/status');
-//});
+// Route::get('/', function () {https://drive.google.com/drive/u/0/folders/1o9fY_8y0e5w20aezfzSjXKbyqBQJW1dC
+//     return view('guest/berandaguest');
+// });
 
-Route::get('/lihatdomisili', function () {
-    return view('kelurahan/lihatdomisili');
-});
+//All
+Route::resource('daftar', buatAkunController::class);
+Route::resource('login', loginController::class);
+//Guest
+Route::resource('/', berandaGuestController::class);
 
-Route::get('/lihatsktm', function () {
-    return view('kelurahan/lihatsktm');
-});
+//Kecamatan
+Route::resource('dashKec', dashKecController::class);
+Route::resource('dashKec2', dashKec2Controller::class);
 
-Route::get('/lihatkitas', function () {
-    return view('kelurahan/lihatkitas');
-});
+//Kelurahan
+Route::resource('antreanDom', antreanDomController::class);
+Route::resource('antreanKred', antreanKredController::class);
+Route::resource('antreanSKTM', antreanSKTMController::class);
+Route::resource('dashKel', dashKelController::class);
+Route::resource('verifDom', verifDomController::class);
+Route::resource('verifKred', verifKredController::class);
+Route::resource('verifSKTM', verifSKTMController::class);
 
-Route::get('/dashboardkec', function () {
-    return view('kecamatan/dashboardkec');
-});
+//Masyarakat
+Route::resource('dashMasy', dashMasyController::class);
+Route::resource('editProfile', editProfileController::class);
+Route::resource('formDom', formDomController::class);
+Route::resource('formKred', formKredController::class);
+Route::resource('formSKTM1', formSKTM1Controller::class);
+Route::resource('formSKTM2', formSKTM2Controller::class);
+Route::resource('kumpulanBerkas', kumpulanBerkasController::class);
+Route::resource('profile', profileController::class);
+Route::resource('riwayat', riwayatController::class);
+Route::resource('riwayatSKTM', riwayatSKTMController::class);
+Route::resource('riwayatDom', riwayatDomController::class);
+Route::resource('riwayatKred', riwayatKredController::class);
 
-Route::get('/dashboardkec2', function () {
-    return view('kecamatan/dashboardkec2');
-});
 
 
-// navbar
-Route::get('/navbaruser', function () {
-    return view('navbar/navbaruser');
-});
-
-Route::get('/navbarguest', function () {
-    return view('navbar/navbarguest');
-});
-
-Route::get('/navbaradmin', function () {
-    return view('navbar/navbaradmin');
-});
-
-Route::get('/navbarkec', function () {
-    return view('navbar/navbarkec');
-});
-
-Route::get('/dashboardmasy', function () {
-    return view('masyarakat/dashboard');
-});
-
-Route::resource('akun', AkunController::class);
-
-Route::resource('antreanSKTM', SKTMController::class);
-
-Route::resource('antreandomisili', DomisiliController::class);
-
-Route::resource('kelurahan', KelurahanController::class);
-
-Route::resource('kecamatan', KecamatanController::class);
-
-Route::resource('kredit', kredit2Controller::class);
 
