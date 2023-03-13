ec29736732438a2daee231145a94dc02b19412a4
renyaw <norarenada@students.undip.ac.id>
2023-03-14 01:11:23 +0700

    sktm 1 done

--- resources/views/masyarakat/formSKTM1.blade.php
+++ resources/views/masyarakat/formSKTM1.blade.php
@@ -14,55 +14,58 @@
     <!-- Context -->
     @section('container')
     <div class="container">
-        <h4 class="text-center mt-3">Surat Keterangan Tidak Mampu</h4>
+        <h4 class="text-center mt-3 fw-bold">Surat Keterangan Tidak Mampu</h4>
+        <h5 class="text-center mt-1">Persyaratan Pelayanan</h5>
     </div>
 
-    <div class="container mt-3">
-        <form action="">
-            <div class="form-group">
-                <div class="tulisan">
-                    <h5 class="text">Keperluan (Silahkan Pilih Salah Satu Layanan untuk Melihat Persyaratan)</h5>
-                </div>
-                <br>
-                    <label class="mt-2">Silahkan Pilih Keperluan Anda Terlebih Dahulu! <br> Persyaratan ini yang perlukan untuk diinput dalam Berkas Lain <br><br></label>
-                    <select class="form-control" id="sell">
-                        <option value="Pilih Keperluan">-- Silahkan Pilih Keperluan --</option>
-                        <option value="KTP Orang Tua, Akta Kelahiran Anak, KKS atau Kartu Perlindungan Sosial Lainnya ">Keperluan Pendidikan</option>
-                        <option value="Surat Rujukan atau SK Rawat Inap, Kartu KIS atau BPJS Keluarga Lain, KKS (jika ada)">Keperluan Keringanan Biaya Inap</option>
-                        <option value="KTP Suami dan Istri, SK Lahir dari Penolong, Surat Rujukan atau SK Rawat inap, KIS atay BPJS Keluarga Lain, KKS (jika ada)">Keperluan Jampersal</option>
-                        <option value="KIS/Kartu BPJS yang hendak dinonaktifkan, Surat Pernyataan dari Dinas Sosial tentang Penonaktifan PBI">Keperluan Penonaktifan Kepersertaan BPJS/JKN-KIS Pemerintah</option>
-                        <option value="KIS/Kartu BPJS, Bukti Pelunasan pembayaran premi JKN-KIS Mandiri">Penonaktifan Kepesertaan BPJS Kesehatan/JKN-KIS Mandiri Kelas III</option>
-                        <option value="Tidak Membutuhkan Berkas Lain">Keperluan Di luar Pendidikan dan Kesehatan</option>
-                    </select>
-                    <br>
-                    <div class="well" id="weli">
-                        <p>Persyaratan yang dibutuhkan adalah : <br></p>
-                    </div>
-                </div>
-        </form>
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        Persyaratan yang tertera di bawah ini, merupakan persyaratan yang harus dimasukkan ke dalam berkas lain, sesuai dengan keperluan urusan.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
 
-            <br>
+    <div class="container mt-4">
+        <p class="fw-bold">A. Surat Keterangan Tidak Mampu yang digunakan untuk Urusan <span class="text-success">Pendidikan</span></p>
+        <ul>
+            <li>KTP Orang Tua</li>
+            <li>Akta Kelahiran Anak yang bersangkutan</li>
+            <li>KKS atau Kartu Perlindungan Sosisal lainnya</li>
+        </ul>
+        <p class="fw-bold">B. Surat Keterangan Tidak Mampu yang digunakan untuk <span class="text-success">Keringanan Biaya Rawat Inap</span></p>
+        <ul>
+            <li>Surat Rujukan atau Surat Keterangan Rawat Inap atau Surat Keterangan Sakit dari RS/Dokter</li>
+            <li>KIS / Kartu BPJS Kesehatan Keluarga Lain (jika ada)</li>
+            <li>KKS (jika ada)</li>
+        </ul>
+        <p class="fw-bold">C. Surat Keterangan Tidak Mampu yang digunakan untuk <span class="text-success">Jampersal</span></p>
+        <ul>
+            <li>KTP Suami dan Istri</li>
+            <li>Surat Keterangan Lahir dari Penolong</li>
+            <li>Surat Rujukan atau Surat Keterangan Rawat Inap atau Surat Keterangan Sakit dari RS/Dokter</li>
+            <li>KIS / Kartu BPJS Kesehatan Keluarga Lain (jika ada)</li>
+            <li>KKS (jika ada)</li>
+        </ul>
+        <p class="fw-bold">D. Surat Keterangan Tidak Mampu yang digunakan untuk <span class="text-success">Penonaktifan Kepesertaan BPJS Kesehatan/JKN-KIS Pemerintah</span></p>
+        <ul>
+            <li>KIS / Kartu BPJS Kesehatan yang hendak dinoanaktifkan</li>
+            <li>Surat Pernyataan di Dinas Sosial tentang Penonaktifan PBI</li>
+        </ul>
+        <p class="fw-bold">E. Surat Keterangan Tidak Mampu yang digunakan untuk <span class="text-success">Penonaktifan Kepesertaan BPJS Kesehatan/JKN-KIS Mandiri Kelas III</span></p>
+        <ul>
+            <li>KIS / Kartu BPJS Kesehatan</li>
+            <li>Bukti pelunasan pembayaran premi JKN-KIS Mandiri</li>
+        </ul>
+        <p class="fw-bold">F. Surat Keterangan Tidak Mampu yang digunakan untuk Urusan <span class="text-success">Di luar Pendidikan dan Kesehatan</span></p>
+        <ul>
+            Tidak Ada Persyaratan Tambahan (Kosongi Berkas Lain)
+        </ul>
+    </div>
 
-        </div>
 
-    <!-- Field Berkas -->
-
-    <!-- Field Berkas End -->
-
-    <div class="col-auto d-flex justify-content-end">
-        <button href="formSKTM2" class="btn btn-outline-success">Next</button>
+    <div class="col-auto d-flex justify-content-end mb-3">
+        <a href="/formSKTM2" type="button" class="btn btn-outline-success">Isi Form</a>
     </div>
     @endsection
     <!-- Context End -->
-        <script>
-            $(document).ready(function(){
-                $("#sell").change(function(){
-                    var nilai = $(this).val();
-                });
-
-                $("p").html("Persyaratan yang dibutuhkan adalah :" + nilai);
-            });
-        </script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
 </html>
